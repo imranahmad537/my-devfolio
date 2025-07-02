232c44350f0752f5f1c59d04d23e47104727b9f1
imranahmad537 <ahmadimrankh99@gmail.com>
2025-07-02 10:46:39 +0500

    Committed

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/", 
   theme: {
     extend: {
       fontFamily: {
         jost: ['Jost', 'sans-serif'],
         overpass: ['Overpass', 'sans-serif'],
-        lato: ['Lato', 'sans-serif']
-      }
-    }
+        lato: ['Lato', 'san-serif']
+      },
+    },
   },
-  plugins: []
-}
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// // import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//    theme: {
-//     extend: {
-//       fontFamily: {
-//         jost: ['Jost', 'sans-serif'],
-//         overpass: ['Overpass', 'sans-serif'],
-//         lato: ['Lato', 'san-serif']
-//       },
-//     },
-//   },
-//   plugins: [react()],
-//   base: process.env.VITE_BASE_PATH || "/"
-// })
+ 
+})
